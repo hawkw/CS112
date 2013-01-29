a57d16f4de4298cdf0c8c3803887b8dce829ffc1
Hawk Weisman <hawk.weisman@gmail.com>
2013-01-29 17:21:20 -0500

    added margin of error to CalcAvg.

--- CalcAvg.java
+++ CalcAvg.java
@@ -7,6 +7,7 @@
 * Calculates the mean, median, and mode for a set of doubles.
 * The doubles are given as command-line arguments for the program.
 */
+	import java.lang.Math;
 	import java.util.Scanner;
 	public class CalcAvg {
 		
@@ -23,6 +24,23 @@
 			}
 			return result/(input.length+1);
 		}
+		
+		/**
+		* sigma
+		* @author	Hawk Weisman
+		* @param	An array of doubles to be calculated.
+		* @return 	The statistical margin of error.
+		*/
+		public static double sigma (double[] input) {
+			
+			double[] calculating = new double [input.length];
+			double result = 0;
+			
+			for (int i = 0; i < input.length; i++) {
+				calculating[i] = Math.pow((input[i] - mean(input)), 2.0);
+			}
+			return Math.sqrt(mean(calculating));
+		}
 			
 		public static void main (String[] argv) {
 			
@@ -36,7 +54,9 @@
 				values[i] = scan.nextDouble();
 			}
 			
-			System.out.println("The arithmatic mean is: " + mean(values) + ".");
+			System.out.println ("The arithmatic mean is: " + mean(values) + ".");
+			
+			System.out.println ("The standard deviation is: " + sigma(values) + ".");
 			
 		}
 	}
